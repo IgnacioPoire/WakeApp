a8c63547496e22c29bacb79cb55850419061eee4
Ignacio Poire <ignaciopoire@gmail.com>
2021-04-06 19:49:18 -0300

    Alarm form

--- app/src/main/java/com/wakeapp/models/Alarm/Alarm.java
+++ app/src/main/java/com/wakeapp/models/Alarm/Alarm.java
@@ -2,21 +2,29 @@ package com.wakeapp.models.Alarm;
 
 import com.google.android.gms.maps.model.LatLng;
 import java.io.Serializable;
+import java.sql.Time;
 
 public class Alarm implements Serializable {
     private static final long serialVersionUID = 1L;
     private boolean isEnabled;
+    private boolean alwaysActive;
     private String name;
     private double lat;
     private double lng;
     private double radius;
+    private Time time;
+    private long interval;
+    private long endTime;
 
     public Alarm(String name, LatLng latLng, double radius) {
         this.isEnabled = true;
+        this.alwaysActive = true;
         this.name = name;
         this.lat = latLng.latitude;
         this.lng = latLng.longitude;
         this.radius = radius;
+        this.time = new Time(43200000);
+        this.endTime = 0;
     }
 
     public String getName() {
@@ -37,18 +45,42 @@ public class Alarm implements Serializable {
     }
 
     public double getRadius() {
-        return radius;
+        return this.radius;
     }
 
-    public void setRadius(double radius) {
-        this.radius = radius;
+    public void setRadius(double value) {
+        this.radius = value;
     }
 
     public boolean getIsEnabled() {
-        return isEnabled;
+        return this.isEnabled;
     }
 
-    public void switchEnabled() {
-        this.isEnabled = !this.isEnabled;
+    public void setIsEnabled(boolean value) {
+        this.isEnabled = value;
+    }
+
+    public boolean getAlwaysActive() {
+        return this.alwaysActive;
+    }
+
+    public void setAlwaysActive(boolean value) {
+        this.alwaysActive = value;
+    }
+
+    public Time getTime() {
+        return this.time;
+    }
+
+    public void setTime(long value) {
+        this.time = new Time(value);
+    }
+
+    public Time getInterval() {
+        return this.time;
+    }
+
+    public void setInterval(long value) {
+        this.endTime = this.time.getTime() + interval;
     }
 }
--- app/src/main/java/com/wakeapp/ui/alarms/AlarmActivity.java
+++ app/src/main/java/com/wakeapp/ui/alarms/AlarmActivity.java
@@ -1,15 +1,36 @@
 package com.wakeapp.ui.alarms;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
+
 import com.wakeapp.R;
 import com.wakeapp.models.Alarm.Alarm;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 public class AlarmActivity extends AppCompatActivity {
+
+    private static Map<String, Long> intervals;
+    static {
+        Map<String, Long> aMap = new HashMap<>();
+        aMap.put("30 Min", 1800000L);
+        aMap.put("1 Hour", 3600000L);
+        aMap.put("2 Hour", 7200000L);
+        aMap.put("3 Hour", 10800000L);
+        aMap.put("6 Hour", 21600000L);
+        intervals = Collections.unmodifiableMap(aMap);
+    }
 
     private ArrayList<Alarm> alarms;
     private Alarm alarm;
@@ -17,11 +38,54 @@ public class AlarmActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        System.out.println(getIntent().getIntExtra("ALARM_ID", 0));
+
         alarms = (ArrayList<Alarm>) getIntent().getSerializableExtra("alarms_list");
-        alarm = alarms.get(getIntent().getIntExtra("id_alarm", 0));
-        EditText e = (EditText) findViewById(R.id.alarm_name);
-        e.setText(alarm.getName());
+        alarm = alarms.get(getIntent().getIntExtra("ALARM_ID", 0));
+        System.out.println("\nHERE: " + alarm + " ");
+
         setContentView(R.layout.alarm_form);
+
+        //Alarm Name
+        EditText alarmName = findViewById(R.id.alarm_name);
+        alarmName.setText(alarm.getName());
+
+        //Always Active
+        SwitchCompat alwaysActive = findViewById(R.id.always_active);
+        alwaysActive.setChecked(alarm.getAlwaysActive());
+
+        //Time
+        final TextView alarmTimeLabel = findViewById(R.id.alarm_time_label);
+        final EditText alarmTime = findViewById(R.id.alarm_time);
+        final TextView alarmIntervalLabel = findViewById(R.id.alarm_interval_label);
+        final Spinner alarmInterval = findViewById(R.id.alarm_interval);
+        alarmTime.setText(alarm.getTime().toString());
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, new ArrayList<>(intervals.keySet()));
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        alarmInterval.setAdapter(adapter);
+
+        if (alwaysActive.isChecked()) {
+            alarmTimeLabel.setVisibility(View.GONE);
+            alarmTime.setVisibility(View.GONE);
+            alarmIntervalLabel.setVisibility(View.GONE);
+            alarmInterval.setVisibility(View.GONE);
+        }
+
+        alwaysActive.setOnCheckedChangeListener(new SwitchCompat.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    alarmTimeLabel.setVisibility(View.GONE);
+                    alarmTime.setVisibility(View.GONE);
+                    alarmIntervalLabel.setVisibility(View.GONE);
+                    alarmInterval.setVisibility(View.GONE);
+                } else {
+                    alarmTimeLabel.setVisibility(View.VISIBLE);
+                    alarmTime.setVisibility(View.VISIBLE);
+                    alarmIntervalLabel.setVisibility(View.VISIBLE);
+                    alarmInterval.setVisibility(View.VISIBLE);
+                }
+            }
+        });
     }
 }
